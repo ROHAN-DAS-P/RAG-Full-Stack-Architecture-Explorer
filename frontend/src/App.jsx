c74cf4ee597aca59f55442b95372a84d7e9c2402
ROHAN DAS P <rohan2003710@gmail.com>
2026-06-29 15:47:04 +0530

    the front end setup is completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-10">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Tailwind v4 is working!
+      </button>
+    </div>
+  )
+}
+
+export default App
